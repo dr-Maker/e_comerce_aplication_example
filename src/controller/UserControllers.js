647e4c3ca8a3ea9dfb9177ad5d14e63275fda3ea
dr-Maker <contacto@jonathanvenegas.cl>
2026-03-08 21:25:42 -0300

    user list, update, getOne

--- src/controller/UserControllers.js
+++ src/controller/UserControllers.js
@@ -26,12 +26,98 @@ const saveUser = async(req, res) =>{
 }
 
 const getUserById = async(req, res) =>{
-     console.log(req.params);
-     return res.status(200).send({message: "success"});
+    try {
+
+        const id = req.params.id;
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({
+                status: "error",
+                message: "Usuario no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            user: user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            status: "error",
+            message: "Error al obtener usuario",
+            error: error.message
+        });
+
+    }
 }
+
+const getAllUsers = async (req, res) => {
+
+    try {
+
+        const users = await User.find();
+
+        res.status(200).json({
+            status: "success",
+            total: users.length,
+            users: users
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            status: "error",
+            message: "Error al obtener usuarios",
+            error: error.message
+        });
+
+    }
+
+};
+
+const updateUser = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+        const data = req.body;
+
+        const userUpdated = await User.findByIdAndUpdate(
+            id,
+            data,
+            { new: true } // devuelve el registro actualizado
+        );
+
+        if (!userUpdated) {
+            return res.status(404).json({
+                status: "error",
+                message: "Usuario no encontrado"
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            user: userUpdated
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            status: "error",
+            message: error.message
+        });
+
+    }
+}
+
 
 
 module.exports = {
     getUserById,
+    getAllUsers,
+    updateUser,
     saveUser
 }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,6 +4,8 @@ var route = express.Router();
 var Conntroller = require('../controller/UserControllers');
 
 route.get("/get_user/:id", Conntroller.getUserById);
+route.get("/get_all_users/", Conntroller.getAllUsers);
 route.post("/save_user", Conntroller.saveUser);
+route.put("/update_user/:id", Conntroller.updateUser)
 
 module.exports = route;
